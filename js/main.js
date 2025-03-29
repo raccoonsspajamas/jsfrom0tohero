29792edc5c10af971d6170fd71fa9705c02fa5de
raccoonsspajamas <zxcodingninja@gmail.com>
2025-03-29 13:18:02 +0100

    operators in JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 'use strict'
 
-const category = 'toys';
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
 
-console.log(`https://someurl.com/${category}/5`)
 
-const user = "Ivan";
-alert(`Привет, ${user}`)
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr++);
+console.log(incr);
+console.log(--decr);
+
+console.log(5 % 2)
+
+console.log(2 * 4 == '8')
+console.log(2 * 4 === '8')
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose)
